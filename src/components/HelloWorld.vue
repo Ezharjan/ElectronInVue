56038b558e6fc7555f54e13ea9c76ef7e4beb6fb
Alexander <ezharjan@aliyun.com>
2020-06-20 21:48:22 +0800

    debugging why preload-js file can't be found

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="hello">
         <h1>{{ msg }}</h1>
+        <input type="file" id="read-file" name="fileContent" @click="ReadLocalFile()" />
+        <input type="text" id="file-path" name="filePath" />
+        <button id="should-read-as-text" @click="changeReadType()">{{readTypeMsg}}</button>
+        <button id="read-file-in-nodejs" @click="ReadLocalFileInNodeJS()">Read Local File</button>
+        <button id="write-file-in-nodejs" @click="WriteLocalFileInNodeJS()">Write Local File</button>
     </div>
 </template>
 
@@ -9,6 +14,71 @@ export default {
     name: 'HelloWorld',
     props: {
         msg: String
+    },
+    data() {
+        return {
+            readTypeMsg: "Read As Text",
+            shouldReadAsText: false
+        }
+    },
+    created() {
+        this.initialize();
+    },
+    methods: {
+        initialize() {
+            this.shouldReadAsText = false;
+        },
+        changeReadType() {
+            this.shouldReadAsText = !this.shouldReadAsText;
+            console.log(this.shouldReadAsText);
+            this.shouldReadAsText && (this.readTypeMsg = 'Read As Data')
+                || ((this.readTypeMsg = 'Read As Text'));
+        },
+        ReadLocalFile() {
+            var fileInput = document.querySelector('#read-file');
+            var that = this;
+            fileInput.onchange = function () {
+                console.log('文件名:', this.value)
+                var filereader = new FileReader();
+                var fileType = this.files[0].type;
+                console.log(that.shouldReadAsText);
+                if (that.shouldReadAsText === true) {
+                    filereader.readAsText(this.files[0], 'gbk');
+                }
+                else {
+                    filereader.readAsDataURL(this.files[0]);
+                }
+                filereader.onload = function () {
+                    if (/^image\[jpeg|png|gif]/.test(fileType)) {
+                        console.log("Read image content: " + this.result);
+                    }
+                    console.log("Content read: " + this.result);
+                }
+            }
+        },
+        ReadLocalFileInNodeJS() {
+            // let filePath = document.querySelector('#file-path').value;
+            try {
+                // console.log(window.readConfig(filePath));
+                console.log(window.readConfig('D:/myText.txt'));
+            } catch (err) {
+                console.log(err);
+                console.error('error occurred while getting the file path from input.');
+            }
+        },
+        WriteLocalFileInNodeJS() {
+            // let filePath = document.querySelector('#file-path').value;
+            window.writeFileConfig('D:/eee.txt', 'hello world', function () {
+                console.log('done!');
+            });
+            // window.writeFileConfig('D:/ttt.txt', 'hello world');
+            // try {
+            //     window.writeFileConfig(filePath, 'Something you want to write in the file.');
+            // } catch (err) {
+            //     console.log(err);
+            //     console.error('error occurred while getting the file path from input.');
+            // }
+        }
     }
 }
 </script>
---
+++ vue.congig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pluginOptions: {
+        electronBuilder: {
+            preload: 'src/preload.js',
+            // Or, for multiple preload files:
+            // preload: { preload: 'src/preload.js', otherPreload: 'src/preload2.js' }
+            preload: { preload: 'src/preload.js' }
+        }
+    }
+}
