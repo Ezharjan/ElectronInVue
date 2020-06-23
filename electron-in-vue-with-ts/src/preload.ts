a67e447c93769ce6f61123429eec4ca2f40211f1
Ezharjan <ezharjan@aliyun.com>
2020-06-23 16:14:22 +0800

    typescript added

---
+++ electron-in-vue-with-ts/src/preload.ts
@@ -0,0 +1,32 @@
+//This file loads only the preloadable things
+//Move this file into the dist_electron folder when you want to run the code below in electron
+
+// preload.js 中使用nodejs
+const { readFileSync } = require('fs');
+const { writeFile } = require('fs');
+
+
+(window as any).readConfig = function (filePath: string) {
+    // globalThis.readConfig = function (filePath: string) {
+    // window.readConfig = function (filePath: string) {
+    console.log("File has been successfully read!");
+    const data = readFileSync(filePath);
+    return data;
+};
+
+
+(window as any).writeFileConfig = function (filePath: string, anythingUWanaWrite: string) {
+    // globalThis.writeFileConfig = function (filePath: string, anythingUWanaWrite: string) {
+    writeFile(filePath, anythingUWanaWrite, function () {
+        console.log("File written done!");
+    });
+};
+//Below is the reference of the code above
+// fs.writeFile(_path, "electron + Javascript", function (err) {
+//     if (!err)
+//         console.log("写入成功！");
+// });
+
+// index.html 或其他vue脚本后加载的内容可以使用window.readConfig()方法，但不能使用Node.js特性
+// console.log(window.readConfig()) // 正常执行
+// console.log(readFileSync('./config.json')) // 报错
